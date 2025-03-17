2db936c822274a11f2ff4d2bdde2f1e066210b6f
Song <songx9241@gmail.com>
2025-03-18 02:17:18 +0900

    01 to 02

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const Title = 'Store'
+export const MenuList = [
+    {href: '/search',text:'Search'},
+    {href: '/account',text:'Account'},
+    {href: '/cart',text:'Cart'},
+]
+
+export const NavList = [
+    {title: 'Categories',list:['Clothing','Audio','Furniture']},
+    {title: 'Collections',list:['Lates Drops','Weekly Picks','Sale']},
+    {title:'Code',list:['GitHub','Documentation','Source code']}
+]
